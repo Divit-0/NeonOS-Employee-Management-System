09ce1acd6065625fef3c8e8e1087fb054f5760af
Divit-0 <gdivit2006@gmail.com>
2026-05-16 19:03:15 +0530

    Fixed production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+
+    baseURL:
+        "https://neonos-employee-management-system.onrender.com/api",
 });
 
 export default API;
